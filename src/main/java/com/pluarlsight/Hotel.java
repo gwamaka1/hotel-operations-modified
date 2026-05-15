bff34ecb8b2b823d7609b3369ef666f809e70d54
gwamaka1 <mwamtobegwamaka@gmail.com>
2026-05-15 09:12:30 -0500

    completed the bookroom method

--- src/main/java/com/pluarlsight/Hotel.java
+++ src/main/java/com/pluarlsight/Hotel.java
@@ -1,4 +1,36 @@
 package com.pluarlsight;
 
 public class Hotel {
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedRooms;
+    private int bookedSuites;
+    Hotel(String name, int numberOfRooms,int numberOfSuites, int bookedRooms,int bookedSuites){
+        this.name = name;
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfSuites = numberOfSuites;
+        bookedRooms = 0;
+        bookedSuites = 0;
+    }
+    Hotel(String name, int numberOfRooms, int numberOfSuites){
+        this.name = name;
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfSuites = numberOfSuites;
+    }
+    public boolean bookRoom(int numberOfRooms, boolean isSuite){
+        if(this.numberOfRooms- this.bookedRooms == 0|| this.numberOfSuites - this.bookedSuites == 0){
+            return false;
+        }
+        if(isSuite){
+            this.bookedSuites += numberOfRooms;
+        }
+        this.bookedRooms+= numberOfRooms;
+        return true;
+
+
+
+
+    }
+
 }
